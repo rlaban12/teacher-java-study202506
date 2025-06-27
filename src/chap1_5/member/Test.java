d127b9b48b4a1a753316f8dfd15d51d62ace6040
soongu <naong526@naver.com>
2025-06-27 10:36:13 +0900

    회원 관리 시스템 생성

---
+++ src/chap1_5/member/Test.java
@@ -0,0 +1,29 @@
+package chap1_5.member;
+
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args) {
+
+//        Member m1 = new Member(30, "abc1234@gmail.com", "1234", "말포이", Gender.MALE);
+//        System.out.println(m1);
+
+        // 클래스 안에 있는 데이터는 반드시 객체생성을 통해 접근함.
+        MemberRepository mr = new MemberRepository();
+
+        // 멤버 추가 테스트
+        mr.addMember(new Member(80, "oldman@gmail.com", "9888", "하라버지", Gender.MALE));
+
+        System.out.println(Arrays.toString(mr.getMembers()));
+        System.out.println(mr.size());
+
+        System.out.println("================");
+
+        Member foundMember = mr.findMemberByEmail("oldman@gmail.com");
+        System.out.println("foundMember = " + foundMember);
+
+        System.out.println("===============");
+        boolean flag = mr.isDuplicateEmail("oldman123@gmail.com");
+        System.out.println("flag = " + flag);
+    }
+}
